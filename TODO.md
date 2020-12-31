2d8e9eb26b9bb1bd6d9ae08df088aade5961c9f8
erdeszt <erdeszt@gmail.com>
2020-12-31 14:02:21 +0100

    Suppress unused functions warning

---
+++ TODO.md
@@ -0,0 +1,4 @@
+* Ignore cppcheck unused warnings for library functions
+* Deviation from MISRA for IO mapped registers
+* USART
+* ADC input
--- check.sh
+++ check.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+OPTS="--enable=all --std=c99 --addon=misra --suppressions-list=suppressions.txt -I../SafeAVR/include"
+
 pushd check
 
 for filename in ../SafeAVR/src/*.c; do
-  cppcheck --enable=all --std=c99 --addon=misra -I../SafeAvr/include ${filename} 
+  cppcheck ${OPTS} ${filename}
 done
 
 for filename in ../Examples/*.c; do
-  cppcheck --enable=all --std=c99 --addon=misra -I../SafeAvr/include ${filename} 
+  cppcheck ${OPTS} ${filename}
 done
 
 popd
