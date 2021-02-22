f46dd1f185bac4187d580ec68a759e90b38fda36
erdeszt <erdeszt@gmail.com>
2021-02-22 20:40:21 +0100

    Remove old comment

--- src/hal/timer.c
+++ src/hal/timer.c
@@ -29,7 +29,6 @@ boolean timer_delay_ms(const u16 delay)
     boolean delays_successful = TRUE;
     u16 delay_count = 0;
 
-    // Loop upper bound is UINT_16_MAX. Is it good enough?
     while (delay_count < SAFEAVR_TIMER_MAX_DELAY && delay_count < delay) {
         delays_successful = timer_delay_1ms();
         delay_count++;
